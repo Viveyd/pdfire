759f06763abe82e13f05829e11c8f8f951715a8b
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-08 10:58:18 +0800

    Add state: selection & input file change handler

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import "./App.css";
 import "./reset.css";
 
 function App() {
+  const [selection, updateSelection] = useState([]);
+
+  function update(e) {
+    const files = [...e.target.files];
+    console.log(files);
+  }
+
   return (
     <>
       <header>
@@ -20,6 +27,7 @@ function App() {
               type="file"
               accept="image/*"
               multiple
+              onChange={update}
             />
           </label>
         </button>
