d3e7b0da204c42bb90ce84360825cd6d77db8a9a
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-08 10:50:30 +0800

    Add file input button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,23 @@ import "./reset.css";
 function App() {
   return (
     <>
-      <header> PDFier</header>
+      <header>
+        <h1>PDFier</h1>
+      </header>
+      <main>
+        <button className="file-btn">
+          <label htmlFor="file-selector" className="center-flex full-wh">
+            Add files
+            <input
+              id="file-selector"
+              className="hidden"
+              type="file"
+              accept="image/*"
+              multiple
+            />
+          </label>
+        </button>
+      </main>
     </>
   );
 }
