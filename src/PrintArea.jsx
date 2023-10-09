c97b5fe3f9a8ec13e6ac897b9bfd0545d6a88c1b
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-09 23:50:17 +0800

    Show images in PrintArea

--- src/PrintArea.jsx
+++ src/PrintArea.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function PrintArea({ data }) {
-  if (!data.length) return;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    Promise.all(data.map(getImageUrl)).then((values) => setImages(values));
+  }, [data]);
 
   return (
     <section id="print-area" className="hidden">
-      {0}
+      {images.length &&
+        images.map((item, index) => <img key={index} src={item} />)}
     </section>
   );
+}
+
+function getImageUrl(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (e) => reject(e);
+    reader.readAsDataURL(file);
+  });
 }
