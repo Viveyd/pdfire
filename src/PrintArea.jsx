46e1e2ecb622f344653dd81dd14ac15ec3bb26f5
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-10 10:50:28 +0800

    Separate PrintArea images into pages

--- src/PrintArea.jsx
+++ src/PrintArea.jsx
@@ -1,7 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PrintArea({ data }) {
   const [images, setImages] = useState([]);
+  const maxPerPage = 6;
+  const pagesData = [[]];
+  for (
+    let imageCounter = 0, pageCounter = 0;
+    imageCounter < images.length;
+    imageCounter++
+  ) {
+    if (pageCounter === maxPerPage) {
+      pagesData.push([]);
+      pageCounter = 0;
+    }
+    pagesData[pagesData.length - 1].push(images[imageCounter]);
+    pageCounter += 1;
+  }
 
   useEffect(() => {
     Promise.all(data.map(getImageUrl)).then((values) => setImages(values));
@@ -10,8 +24,20 @@ export default function PrintArea({ data }) {
   return (
     <section id="print-area" className="hidden">
       {images.length &&
-        images.map((item, index) => <img key={index} src={item} />)}
+        pagesData.map((pageData, index) => (
+          <PrintPage key={index} data={pageData} />
+        ))}
     </section>
+  );
+}
+
+function PrintPage({ data }) {
+  return (
+    <div className="print-page">
+      {data.map((item, index) => (
+        <img key={index} src={item} />
+      ))}
+    </div>
   );
 }
 
